ae6922c155af3992b65bcd751ac6012c0a84d4ac
VaishnaviMoolya <vaishnavi.moolya25@ds.sce.edu.in>
2026-08-08 19:27:02 +0530

    Update timepass.sql

--- timepass.sql
+++ timepass.sql
@@ -2,8 +2,8 @@ create database student1;
 use student1;
 create table student1(sid int, sname varchar(10));
 insert into student1 values(10 , 'parisa');
-select * from student1;
 insert into student1 values(11,'Dhanashri');
+select * from student1;
 
 
 
