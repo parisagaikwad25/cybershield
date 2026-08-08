0b1f4d049b883d6ee52c9291394fea9f53ef7502
dhanashrinemade25-dot <dhanashri.nemade25@ds.sce.edu.in>
2026-08-08 19:26:22 +0530

    Update timepass.sql

--- timepass.sql
+++ timepass.sql
@@ -3,7 +3,7 @@ use student1;
 create table student1(sid int, sname varchar(10));
 insert into student1 values(10 , 'parisa');
 select * from student1;
-insert into student1(11,'Dhanashri');
+insert into student1 values(11,'Dhanashri');
 
 
 
