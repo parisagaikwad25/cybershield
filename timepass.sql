c0291931665a11fa8331116e2aa449a6f58830a5
Parisa G <parisa.gaikwad25@ds.sce.edu.in>
2026-08-12 20:23:39 +0530

    Delete timepass.sql

--- timepass.sql
+++
@@ -1,9 +0,0 @@
-create database student1;
-use student1;
-create table student1(sid int, sname varchar(10));
-insert into student1 values(10 , 'parisa');
-insert into student1 values(11,'Dhanashri');
-select * from student1;
-
-
-
