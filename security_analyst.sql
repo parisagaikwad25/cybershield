e00801a112cc844eb64c031a6f4c4a98346ecdf1
dhanashrinemade25-dot <dhanashri.nemade25@ds.sce.edu.in>
2026-08-18 12:16:06 +0530

    Update security_analyst.sql

--- security_analyst.sql
+++ security_analyst.sql
@@ -1,35 +1,38 @@
-create database analyst1;
 
-use analyst1;
+CREATE DATABASE analyst1;
+USE analyst1;
 
-create table security_analysts(
-    analyst_id int primary key,
-    analyst_name varchar(50),
-    contact_details varchar(100)
+CREATE TABLE security_analysts (
+    analyst_id INT PRIMARY KEY,
+    analyst_name VARCHAR(50) NOT NULL,
+    contact_details VARCHAR(100),
+
+    FOREIGN KEY (analyst_id)
+        REFERENCES emp2.emp2(emp_id)
 );
 
-insert into security_analysts values
-(2001, 'Aarav Sharma', 'aarav.sharma@example.com, 9876543210');
+INSERT INTO security_analysts VALUES
+(001, 'Aarav Sharma', 'aarav.sharma@example.com, 9876543210');
 
-insert into security_analysts values
-(2002, 'Priya Nair', 'priya.nair@example.com, 9865432107');
+INSERT INTO security_analysts VALUES
+(002, 'Priya Nair', 'priya.nair@example.com, 9865432107');
 
-insert into security_analysts values
-(2003, 'Vikram Singh', 'vikram.singh@example.com, 9856123470');
+INSERT INTO security_analysts VALUES
+(011, 'Vikram Singh', 'vikram.singh@example.com, 9856123470');
 
-insert into security_analysts values
-(2004, 'Meera Menon', 'meera.menon@example.com, 9845012367');
+INSERT INTO security_analysts VALUES
+(014, 'Meera Menon', 'meera.menon@example.com, 9845012367');
 
-insert into security_analysts values
-(2005, 'Kavya Iyer', 'kavya.iyer@example.com, 9834567120');
+INSERT INTO security_analysts VALUES
+(010, 'Kavya Iyer', 'kavya.iyer@example.com, 9834567120');
 
-insert into security_analysts values
-(2006, 'Arjun Deshmukh', 'arjun.deshmukh@example.com, 9823456710');
+INSERT INTO security_analysts VALUES
+(009, 'Arjun Deshmukh', 'arjun.deshmukh@example.com, 9823456710');
 
-insert into security_analysts values
-(2007, 'Ishita Shah', 'ishita.shah@example.com, 9812345670');
+INSERT INTO security_analysts VALUES
+(012, 'Ishita Shah', 'ishita.shah@example.com, 9812345670');
 
-insert into security_analysts values
-(2008, 'Karan Malhotra', 'karan.malhotra@example.com, 9801234567');
+INSERT INTO security_analysts VALUES
+(013, 'Karan Malhotra', 'karan.malhotra@example.com, 9801234567');
 
-select * from security_analysts;
+SELECT * FROM security_analysts;
